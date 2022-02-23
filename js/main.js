707166e0e161022db99fe4526279186ce6e60132
LuchiAdaobi <Luchichronicles@gmail.com>
2022-02-23 09:52:51 +0100

    fixed styling issues in pop-up

--- js/main.js
+++ js/main.js
@@ -9,9 +9,7 @@ const about = document.getElementById('about');
 about.addEventListener('mouseenter', () => {
     coaches.classList.add('show');
 });
-// about.addEventListener('mouseleave', () => {
-//     coaches.classList.remove('show');
-// });
+
 
 coaches.addEventListener('mousehover', () => {
     coaches.classList.add('show');
@@ -21,7 +19,7 @@ coaches.addEventListener('mouseleave', () => {
 });
 
 
-        // HAMBURGER MENU
+// HAMBURGER MENU
 
 
 const closeBtn = document.getElementById('close-btn');
@@ -57,7 +55,7 @@ function clickOutside(e) {
 const  popUp = document.getElementById('popup');
 const  close = document.getElementById('popup-close');
 const  button = document.querySelectorAll('button');
-// const  popupContent = document.getElementById('popup-content');
+
 
 document.querySelectorAll('.button').forEach(i => i.addEventListener(
     'click',
@@ -81,78 +79,3 @@ function clickOutside(e) {
     }
     
 }
-
-
-    // TOGGLE DROPDOWN
-
-    // const angleUp = document.getElementById('angle-up');
-    // const angleDown = document.getElementById('angle-down');
-    // const coaches2 = document.getElementById('coaches');
-    
-
-    // angleUp.addEventListener('click', () => {
-    //     coaches2.classList.add('show');
-    //     angleUp.classList.add('inactive');
-    //     angleDown.classList.add('active');
-    // });
-
-    // angleDown.addEventListener('click', () => {
-    //     coaches2.classList.remove('show');
-    //     angleUp.classList.remove('inactive');
-    //     angleDown.classList.remove('active');
-    // });
-
-
-    // TOGGLE DROPDOWN
-    // const angleUp = document.querySelectorAll('angle-up');
-    // const angleDown = document.querySelectorAll('angle-down');
-    // const coaches2 = document.getElementById('coaches');
-    
-
-    // document.querySelectorAll('#angle-up').forEach(i => i.addEventListener(
-    //     'click',
-    //     e => {
-    //         coaches2.classList.add('show');
-    //     angleUp.classList.add('inactive');
-    //     angleDown.classList.add('active');
-        
-    //     }
-    // ));
-
-    // document.querySelectorAll('#angle-down').forEach(i => i.addEventListener(
-    //     'click',
-    //     e => {
-    //         coaches2.classList.add('show');
-    //     angleUp.classList.add('inactive');
-    //     angleDown.classList.add('active');
-    //     }
-    // ));
-
-
-   
-
-  
-
-// REMOVE HOVER FROM MENU
-// const coaches3 = document.getElementById('coaches');
-// const about2 = document.getElementById('about');
-// const mediaQuery = window.matchMedia("(max-width: 1150px)")
-
-
-
-// function removeHover(e) {
-//     if(e.matches) {
-//         about2.addEventListener('mouseenter', () => {
-//             coaches3.classList.remove('show');
-//         });
-       
-       
-//     }
-// }
-// mediaQuery.addListener(removeHover)
-
-// removeHover(mediaQuery)
-
-
-
-    
